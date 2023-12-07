1a381444b78d9aed636e400ea2452544c774a54a
jlipovetz <JLIPOVETZ@GMAIL.COM>
2023-12-07 09:37:41 -0600

    reset

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         <h1>Joe Lipovetz Portfolio!</h1>
         <a href="/about">About</a>
         <a href="/contact">Contact</a>
-        <a href="/portfolio" target="_self">Portfolio</a>
+        <a href="/portfolio">Portfolio</a>
         <a href="https://github.com/jlipovetz/" target="_blank">GitHub</a>
 
       </header>
